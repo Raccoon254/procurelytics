339763d6063c883b79d0381e08a5cd9b55c1136b
Steve Tom <tomsteve187@gmail.com>
2023-07-11 08:46:52 +0300

    fixed charts and added apex charts as a replacement to chart js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <!-- <script src="{{ asset('node_modules/chart.js/dist/chart.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.min.js" integrity="sha512-mlz/Fs1VtBou2TrUkGzX4VoGvybkD9nkeXWJm3rle0DPHssYYx4j+8kIS15T78ttGfmOjH0lLaBXGcShaVkdkg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
         <!-- Fonts -->
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
--- resources/views/procurement/visualize.blade.php
+++ resources/views/procurement/visualize.blade.php
@@ -5,112 +5,71 @@
 @section('content')
     <div class="flex flex-col md:flex-row">
         <div class="w-full md:w-1/2 p-3">
-            <div class="chart-container h-64 md:h-auto">
-                <canvas id="procurementChart"></canvas>
-            </div>
+            <div class="chart-container h-64 md:h-auto" id="procurementChart"></div>
         </div>
         <div class="w-full md:w-1/2 p-3">
-            <div class="chart-container h-64 md:h-auto">
-                <canvas id="procurementChartFirmCategory"></canvas>
-            </div>
+            <div class="chart-container h-64 md:h-auto" id="procurementChartFirmCategory"></div>
         </div>
     </div>
     <div class="flex flex-col md:flex-row">
         <div class="w-full md:w-1/2 p-3">
-            <div class="chart-container h-64 md:h-auto">
-                <canvas id="procurementChartSpendCategory"></canvas>
-            </div>
+            <div class="chart-container h-64 md:h-auto" id="procurementChartSpendCategory"></div>
         </div>
         <div class="w-full md:w-1/2 p-3">
-            <div class="chart-container h-64 md:h-auto">
-                <canvas id="procurementPieChart"></canvas>
-            </div>
+            <div class="chart-container h-64 md:h-auto" id="procurementPieChart"></div>
         </div>
     </div>
 
     <script>
-        var ctx = document.getElementById('procurementChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: [@foreach($procurements as $proc) "{{ $proc->created_at->format('Y-m-d') }}", @endforeach],
-                datasets: [{
-                    label: 'Amount',
-                    data: [@foreach($procurements as $proc) {{ $proc->amount }}, @endforeach],
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    fill: false,
-                    tension: 0.3
-                }]
+        var procurementChart = new ApexCharts(document.querySelector("#procurementChart"), {
+            chart: {
+                type: 'line'
             },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
+            series: [{
+                name: 'Amount',
+                data: [@foreach($procurements as $proc) {{ $proc->amount }}, @endforeach]
+            }],
+            xaxis: {
+                categories: [@foreach($procurements as $proc) "{{ $proc->created_at->format('Y-m-d') }}", @endforeach]
             }
         });
+        procurementChart.render();
 
-        var ctxFirmCategory = document.getElementById('procurementChartFirmCategory').getContext('2d');
-        var labelsFirmCategory = [@foreach($procurements as $proc) "{{ $proc->firm_name }} - {{ $proc->category->name }}", @endforeach];
-        var dataFirmCategory = [@foreach($procurements as $proc) {{ $proc->amount }}, @endforeach];
-        var myChartFirmCategory = new Chart(ctxFirmCategory, {
-            type: 'line',
-            data: {
-                labels: labelsFirmCategory,
-                datasets: [{
-                    label: 'Amount',
-                    data: dataFirmCategory,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    fill: false,
-                    tension: 0.3
-                }]
+        var procurementChartFirmCategory = new ApexCharts(document.querySelector("#procurementChartFirmCategory"), {
+            chart: {
+                type: 'line'
             },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
+            series: [{
+                name: 'Amount',
+                data: [@foreach($procurements as $proc) {{ $proc->amount }}, @endforeach]
+            }],
+            xaxis: {
+                categories: [@foreach($procurements as $proc) "{{ $proc->firm_name }} - {{ $proc->category->name }}", @endforeach]
             }
         });
+        procurementChartFirmCategory.render();
 
-        var ctxSpendCategory = document.getElementById('procurementChartSpendCategory').getContext('2d');
-        var labelsSpendCategory = [@foreach($procurements as $proc) "{{ $proc->firm_name }} - {{ $proc->spendCategory->name }}", @endforeach];
-        var dataSpendCategory = [@foreach($procurements as $proc) {{ $proc->amount }}, @endforeach];
-        var myChartSpendCategory = new Chart(ctxSpendCategory, {
-            type: 'line',
-            data: {
-                labels: labelsSpendCategory,
-                datasets: [{
-                    label: 'Amount',
-                    data: dataSpendCategory,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    fill: false,
-                    tension: 0.3
-                }]
+        var procurementChartSpendCategory = new ApexCharts(document.querySelector("#procurementChartSpendCategory"), {
+            chart: {
+                type: 'line'
             },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
+            series: [{
+                name: 'Amount',
+                data: [@foreach($procurements as $proc) {{ $proc->amount }}, @endforeach]
+            }],
+            xaxis: {
+                categories: [@foreach($procurements as $proc) "{{ $proc->firm_name }} - {{ $proc->spendCategory->name }}", @endforeach]
             }
         });
+        procurementChartSpendCategory.render();
 
-        var ctxPie = document.getElementById('procurementPieChart').getContext('2d');
-        var labelsPie = [@foreach($procurements as $proc) "{{ $proc->firm_name }}", @endforeach];
-        var dataPie = [@foreach($procurements as $proc) {{ $proc->amount }}, @endforeach];
-        var myPieChart = new Chart(ctxPie, {
-            type: 'pie',
-            data: {
-                labels: labelsPie,
-                datasets: [{
-                    data: dataPie,
-                    backgroundColor: ['#ff6384', '#36a2eb', '#cc65fe', '#ffce56', '#9CCC65', '#26C6DA'],
-                }]
+        var procurementPieChart = new ApexCharts(document.querySelector("#procurementPieChart"), {
+            chart: {
+                type: 'pie'
             },
+            labels: [@foreach($procurements as $proc) "{{ $proc->firm_name }}", @endforeach],
+            series: [@foreach($procurements as $proc) {{ $proc->amount }}, @endforeach]
         });
+        procurementPieChart.render();
     </script>
 @endsection
