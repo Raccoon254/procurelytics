67480e42245a75bedbd9011ef69c39eabe3e1dc8
Steve Tom <tomsteve187@gmail.com>
2023-07-11 12:41:47 +0300

    Styles to nav

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,25 +7,65 @@
         <label for="my-drawer" class="drawer-overlay"></label>
         <div class="menu p-4 w-64 h-full bg-base-200 text-base-content flex flex-col justify-center items-start">
             <!-- Sidebar content here -->
-            <button class="btn btn-primary mb-4">
-                <a href="{{ route('procurement.create') }}" >Create Procurement</a> 
-            </button>
-            <button class="btn btn-primary mb-4">
+
+
+            <a class="flex gap-2 items-center hover:bg-accent p-2 rounded w-full bg-gray-200" href="{{ route('procurement.create') }}" >
+                <i class="fa-solid fa-file-arrow-up"></i>
+                <div class="">
+                    Create Procurement
+                </div>
+            </a>
+
+            <a class="flex gap-2 items-center hover:bg-accent p-2 rounded w-full bg-gray-200" href="{{ route('categories.index') }}" >
+                <i class="fa-solid fa-file-arrow-up"></i>
+                <div class="">
+                    categories
+                </div>
+            </a>
+
+            <a class="flex gap-2 items-center hover:bg-accent p-2 rounded w-full bg-gray-200" href="{{ route('procurement.create') }}" >
+                <i class="fa-solid fa-file-arrow-up"></i>
+                <div class="">
+                    Create Procurement
+                </div>
+            </a>
+
+            <a class="flex gap-2 items-center hover:bg-accent p-2 rounded w-full bg-gray-200" href="{{ route('procurement.create') }}" >
+                <i class="fa-solid fa-file-arrow-up"></i>
+                <div class="">
+                    Create Procurement
+                </div>
+            </a>
+
+            <a class="flex gap-2 items-center hover:bg-accent p-2 rounded w-full bg-gray-200" href="{{ route('procurement.create') }}" >
+                <i class="fa-solid fa-file-arrow-up"></i>
+                <div class="">
+                    Create Procurement
+                </div>
+            </a>
+
+            <a class="flex gap-2 items-center hover:bg-accent p-2 rounded w-full bg-gray-200" href="{{ route('procurement.create') }}" >
+                <i class="fa-solid fa-file-arrow-up"></i>
+                <div class="">
+                    Create Procurement
+                </div>
+            </a>
+            <button class="btn btn-gost mb-2 w-full">
                 <a href="{{ route('categories.index') }}" >Categories</a>
             </button>
-            <button class="btn btn-primary mb-4">
+            <button class="btn btn-gost mb-2 w-full">
                 <a href="{{ route('spend.index') }}" >Spend Categories</a>
             </button>
-            <button class="btn btn-primary mb-4">
+            <button class="btn btn-gost mb-2 w-full">
                 <a href="{{ route('procurement.success') }}" >Procurement Success</a>
             </button>
-            <button class="btn btn-primary mb-4">
+            <button class="btn btn-gost mb-2 w-full">
                 <a href="{{ route('procurement-data') }}" >Procurement Data</a>
             </button>
-            <button class="btn btn-primary mb-4">
+            <button class="btn btn-gost mb-2 w-full">
                 <a href="{{ route('visualize') }}" >Visualize</a>
             </button>
-            <button class="btn btn-primary mb-4" >
+            <button class="btn btn-gost mb-2 w-full" >
                 <a href="{{ route('procurement.show', ['procurement' => 'procurement_id']) }}" >Show Procurement</a>
             </button>
         </div>
